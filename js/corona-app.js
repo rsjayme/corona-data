aea8731951b31bd9af8ce0fd5081847992efc94b
rsjayme <49323686+rsjayme@users.noreply.github.com>
2020-03-20 08:43:59 -0300

    Revert "0.7 WIP"
    This reverts commit 2f0e608314f538e83ab0b52b9a0cac4b18de1ea6.

--- js/corona-app.js
+++ js/corona-app.js
@@ -1,8 +1,10 @@
-const countryInput = document.querySelector('#countryInput');
-const searchBtn = document.querySelector('#searchBtn');
-// TODO LIST
-// !!!! ARRUMAR OS BOTOES DO NAV BAR
-// Adicionar o campo country_pt em todos os dados
+
+
+
+requestData().then(async (data) => { 
+    data = fixData(data);
+    const fullData = data.fullData;
+    data = data.lastData;
 
     // MOSTRAM A MESMA COISA
     //
@@ -19,11 +21,11 @@ const searchBtn = document.querySelector('#searchBtn');
     // })
 
 
-requestData().then(async (data) => { 
-    data = fixData(data);
-    //const fullData = data.fullData;
-    data = data.lastData;
 
+    const totalConfirmed = getTotalConfirmed(data);
+    const totalRecovered = getTotalRecovered(data);
+    const totalDeaths = getTotalDeaths(data);
+    const realDeathsPercent = parseFloat((totalDeaths / (totalRecovered + totalDeaths)) * 100).toFixed(2); 
 
     const breadCrumbs = document.querySelectorAll('.letterItem');
     breadCrumbs.forEach((breadCrumb) => {
@@ -34,15 +36,43 @@ requestData().then(async (data) => {
         });
     });
 
-    renderHome(data);
+    
+    const dataDate = new Date(data[0].date);
+    const month = ("0" + (dataDate.getMonth() + 1)).slice(-2);
+    document.querySelector('#col1').innerHTML = `
+        <p> Estamos com um total de ${totalConfirmed} casos confirmados no mundo. </p>
+        <p> ${totalRecovered} pessoas se recuperaram.. </p>
+        <p> ${totalDeaths} pessoas que morreram. São ${realDeathsPercent}% de todos os casos concluidos (Recuperados ou mortos).
+        <span class='data-info'>Dados de ${dataDate.getDate()}/${month}/${dataDate.getFullYear()}</span>
+    `;
     
 
-    countryInput.addEventListener('input', (e) => {  
-        const input = e.target.value.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
-        if(e.target.value)     
-            renderCountryList(data, input);
-        else
-            renderHome(data);
+
+    searchBtn.addEventListener('click', (e) => {
+        e.preventDefault();     
+
+        requestCountriesJson().then((countriesJSON) => {
+            countriesJSON.find((countryFiltered) => {
+                const inputNormalized = countryInput.value.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
+                const dataNormalized = countryFiltered.nome_pais.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
+                console.log(dataNormalized);
+                if(dataNormalized === inputNormalized) {
+                    countryInput.value = countryFiltered.nome_pais_int;
+                }
+            })
+
+            const txtInput = countryInput.value;
+            const countryData = getDataByCountry(data, txtInput);
+   
+
+            if(countryData) {
+                renderCountryList(getCountryByLetter(txtInput[0], data))
+                renderCountrySummary(countryData, document.querySelector('#col2'));
+            } else
+            {
+                document.querySelector('#alert').style.display = 'flex';
+            }
+        })
     });
 
 
--- js/corona-functions.js
+++ js/corona-functions.js
@@ -93,54 +93,28 @@ const getCountryByLetter = (letter, data) => {
     return filteredList;
 }
 
-const renderHome = (data) => {
-    const totalConfirmed = getTotalConfirmed(data);
-    const totalRecovered = getTotalRecovered(data);
-    const totalDeaths = getTotalDeaths(data);
-    const realDeathsPercent = parseFloat((totalDeaths / (totalRecovered + totalDeaths)) * 100).toFixed(2); 
-    
-    const dataDate = new Date(data[0].date);
-    const month = ("0" + (dataDate.getMonth() + 1)).slice(-2);
-    document.querySelector('#col1').innerHTML = `
-        <p> Estamos com um total de ${totalConfirmed} casos confirmados no mundo. </p>
-        <p> ${totalRecovered} pessoas se recuperaram.. </p>
-        <p> ${totalDeaths} pessoas que morreram. São ${realDeathsPercent}% de todos os casos concluidos (Recuperados ou mortos).
-        <span class='data-info'>Dados de ${dataDate.getDate()}/${month}/${dataDate.getFullYear()}</span>
-    `;
-}
+const renderCountryList = (countryList) => {
 
-const renderCountryList = (countryList, filter) => {
-    if(!filter) return;
     document.querySelector('#title').textContent = 'Lista de paises';
     const contentEl1 = document.querySelector('#col1');
     const contentEl2 = document.querySelector('#col2');
     contentEl1.textContent = '';
-    let countryListFiltered = [];
-
-    countryList.forEach((country) => {
-        if(country.country.toLowerCase().includes(filter.toLowerCase())) {
-            countryListFiltered.push(country);
-        }
-    });
-
-
-        countryListFiltered.forEach((country) => {
-            const textEl = document.createElement('a');
-            textEl.textContent = country.country;
-            textEl.setAttribute('href','#');
-            textEl.addEventListener('click', (e) => {
-                const myCountry = countryListFiltered.find((country) => {
-                    if(country.country === e.target.text) {
-                        return country;
-                    }
-                });
-                renderCountrySummary(myCountry, contentEl2);
-            })
-
-            contentEl1.appendChild(textEl);
-    });
     
-     
+    countryList.forEach((country) => {
+        const textEl = document.createElement('a');
+        textEl.textContent = country.country;
+        textEl.setAttribute('href','#');
+        textEl.addEventListener('click', (e) => {
+            const myCountry = countryList.find((country) => {
+                if(country.country === e.target.text) {
+                    return country;
+                }
+            });
+            renderCountrySummary(myCountry, contentEl2);
+        })
+
+        contentEl1.appendChild(textEl);
+    })
 }
 
 
@@ -156,3 +130,13 @@ const renderCountrySummary = (countryData, element) => {
 };
 
 
+const translateNames = (data, dataNames) => {
+    data.forEach((data) => {
+        dataNames.forEach((dataNames) => {
+            if(data.country === dataNames.nome_pais_int) {
+                data.country = dataNames.nome_pais;
+            }
+        })
+    })
+    return data;
+}
